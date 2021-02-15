87da1cbd16e804df879061ce3c177d2777c05161
Dharmendra Kumar Gupta <48489043+Dharmendra-kumar-gupta@users.noreply.github.com>
2021-02-15 20:11:11 +0530

    Add files via upload

---
+++ browser.py
@@ -0,0 +1,23 @@
+import youtube_dl
+import urllib
+import shutil
+import webbrowser
+import tkinter
+from tkinter import filedialog
+from tkinter import *
+import webbrowser
+from googlesearch import search
+root=Tk()
+root.geometry('500x500')
+root.title('Browser')
+root.configure(bg='#fcfcec')
+L1=Label(root,text="Enter what you want to search :- ",bg='#fcfcec',fg="#4b7fa4",font=("times", 20))
+e=Entry(root,width=100,bg="white",fg="#cb464e",border=10)
+def pas():
+	for j in search(e.get(),tld="co.in", num=6, stop=6, pause=2):
+		webbrowser.open(j)
+mybutton=Button(root,text="Search",bg='#cb464e',fg="#c3c3c3",command=pas,font=("times",20))
+L1.grid(row=0,column=0)
+e.grid(row=1,column=1)
+mybutton.grid(row=2,column=0)
+root.mainloop()
